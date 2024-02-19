718aa259b4879f99a1cd10253b3a3f50468e1b80
nmt0512 <thieu8e@gmail.com>
2024-02-19 15:10:29 +0700

    update-base-url

--- app/src/main/java/com/example/grocerystoretest/network/RetrofitClient.kt
+++ app/src/main/java/com/example/grocerystoretest/network/RetrofitClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "https://b16a-2401-d800-5140-3d77-4eb1-acf5-4a5f-673c.ngrok-free.app" + "/api/"
+    private const val BASE_URL = "https://driving-sweeping-joey.ngrok-free.app" + "/api/"
 
     val instance: ApiService by lazy {
         Retrofit.Builder()
