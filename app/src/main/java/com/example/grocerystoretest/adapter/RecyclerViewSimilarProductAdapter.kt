7e06b592981a9bb5fa76a9aa73c360ef7dea4d47
nmt0512 <thieu8e@gmail.com>
2024-06-11 20:12:45 +0700

    feat: add similar product list

---
+++ app/src/main/java/com/example/grocerystoretest/adapter/RecyclerViewSimilarProductAdapter.kt
@@ -0,0 +1,74 @@
+package com.example.grocerystoretest.adapter
+
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.grocerystoretest.R
+import com.example.grocerystoretest.databinding.ItemHomeProductBinding
+import com.example.grocerystoretest.model.response.product.ProductResponse
+import com.example.grocerystoretest.utils.NumberConverterUtil
+import com.example.grocerystoretest.view.ProductDetailActivity
+
+class RecyclerViewSimilarProductAdapter(private val productResponseList: List<ProductResponse>) :
+    RecyclerView.Adapter<RecyclerViewSimilarProductAdapter.SimilarProductViewHolder>() {
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): RecyclerViewSimilarProductAdapter.SimilarProductViewHolder {
+        val view = LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.item_home_product, parent, false)
+        val binding = ItemHomeProductBinding.bind(view)
+        return SimilarProductViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return productResponseList.size
+    }
+
+    override fun onBindViewHolder(
+        holder: RecyclerViewSimilarProductAdapter.SimilarProductViewHolder,
+        position: Int
+    ) {
+        holder.bind(productResponseList[position])
+    }
+
+    inner class SimilarProductViewHolder(val binding: ItemHomeProductBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(productResponse: ProductResponse) {
+            binding.txtName.text = productResponse.name
+            if (productResponse.images.isNotEmpty()) {
+                Glide.with(binding.root)
+                    .load(productResponse.images[0])
+                    .centerInside()
+                    .into(binding.imgProduct)
+            }
+            val priceStr =
+                NumberConverterUtil.getProductPriceStringByPrice(productResponse.unitPrice)
+            binding.txtPrice.text = priceStr
+
+            binding.btnAddToCart.visibility = View.GONE
+
+            binding.imgProduct.setOnClickListener {
+                startProductDetailActivity(productResponse.id)
+            }
+            binding.txtName.setOnClickListener {
+                startProductDetailActivity(productResponse.id)
+            }
+            binding.txtPrice.setOnClickListener {
+                startProductDetailActivity(productResponse.id)
+            }
+        }
+
+        private fun startProductDetailActivity(productId: Int) {
+            val intent = Intent(binding.root.context, ProductDetailActivity::class.java)
+            intent.putExtra("productId", productId)
+            binding.root.context.startActivity(intent)
+        }
+    }
+}
